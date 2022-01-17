3150a3ef394749ba1f96294a78f7542c6dfdc58b
kasi423 <ch.kasirao023@gmail.com>
2022-01-17 13:51:24 +0530

    added ec2 file

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "main" {
-  ami           = "ami-08e4e35cccc6189f4"
+  ami           = ""
   key_name      = "tower43"
-  instance_type = "t2.micro"
+  instance_type = ""
   tags= {
     Name = "jenkins_instance"
   }
