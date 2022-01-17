4a816d01aecba9e2bbb834c56ed4c4050ff392ef
kasi423 <ch.kasirao023@gmail.com>
2022-01-17 17:52:59 +0530

    change the key name

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "main" {
   ami           = "ami-0af25d0df86db00c1"
-  key_name      = "tower43"
+  key_name      = "key56"
   instance_type = "t2.micro"
   tags= {
     Name = "jenkins_instance"
