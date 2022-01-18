fe6599f6de90aa065f1fd42ff714d2cc1cd248a0
kasi423 <38430122+kasi423@users.noreply.github.com>
2022-01-18 17:24:30 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "main" {
-  ami           = "ami-0a2306ef347189603"
+  ami           = "ami-08e4e35cccc6189f4"
   key_name      = "key56"
   instance_type = "t2.micro"
   tags= {
