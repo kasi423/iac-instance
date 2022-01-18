883aa887b12d946d18f67d2ca4c829d3690e365b
kasi423 <38430122+kasi423@users.noreply.github.com>
2022-01-18 17:27:42 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "main" {
   ami           = "ami-001089eb624938d9f"
-  key_name      = "key56"
+  key_name      = "tower34"
   instance_type = "t2.micro"
   tags= {
     Name = "jenkins_instance"
