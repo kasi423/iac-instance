2247c5a4029aa736db2704b8c8e2948db9e274c0
kasi423 <38430122+kasi423@users.noreply.github.com>
2022-01-19 16:54:28 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,3 +1,7 @@
+resource "aws_ami_launch_permission" "main" {
+  image_id   = "ami-001089eb624938d9f"
+  account_id = "397337740137"
+}
 resource "aws_instance" "main" {
   ami           = "ami-001089eb624938d9f"
   key_name      = "tower34"
