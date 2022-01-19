39077eb0774e72fd20f7ab1c449ed3e66dafc6de
kasi423 <38430122+kasi423@users.noreply.github.com>
2022-01-19 23:31:51 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,12 +1,20 @@
-resource "aws_ami_launch_permission" "main" {
-  image_id   = "ami-001089eb624938d9f"
-  account_id = "397337740137"
-}
-resource "aws_instance" "main" {
-  ami           = "ami-001089eb624938d9f"
-  key_name      = "tower34"
-  instance_type = "t2.micro"
-  tags= {
-    Name = "jenkins_instance"
-  }
+#resource "aws_ami_launch_permission" "main" {
+#  image_id   = "ami-001089eb624938d9f"
+#  account_id = "397337740137"
+#}
+#resource "aws_instance" "main" {
+#  ami           = "ami-001089eb624938d9f"
+#  key_name      = "tower34"
+#  instance_type = "t2.micro"
+#  tags= {
+#    Name = "jenkins_instance"
+#  }
+#}
+resource "aws_ec2_host" "test" {
+  instance_type     = "t2.micro"
+  ami               = "ami-08e4e35cccc6189f4"
+  key_name          = "tower43"
+  availability_zone = "us-east-1a"
+  host_recovery     = "on"
+  auto_placement    = "on"
 }
