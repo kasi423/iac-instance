ad39055a5aca8d120192ea4801a5cffc4d9ebb4f
kasi423 <38430122+kasi423@users.noreply.github.com>
2022-01-19 23:48:18 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -22,7 +22,7 @@ resource "aws_instance" "http" {
   ami           = "ami-08e4e35cccc6189f4"
   instance_type = "t2.micro"
   #key_name      = aws_key_pair.user_key.key_name
-  vpc_security_group_ids = "sg-063368c2a0b91deed"
+  vpc_security_group_ids = ["sg-063368c2a0b91deed"]
   subnet_id = "subnet-0c96eb1b3abaa2adc"
   tags = {
     Name = "http-instance"
