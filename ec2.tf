d5b664b71688433ddc8dec23e80318e0b637a9a4
kasi423 <38430122+kasi423@users.noreply.github.com>
2022-01-20 00:00:32 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -20,7 +20,7 @@
 provider "aws" {
   access_key  = "AKIAVZAZMYNURSFQQPO2"
   secret_key  = "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"
-  region = "us-east-1"
+  region      = "us-east-1"
 }
 
 resource "aws_instance" "http" {
